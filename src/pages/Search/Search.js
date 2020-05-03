7ef78598d4be25a2fb0cbf45f93cee5f62872c73
Javi <rotegras@gmail.com>
2020-05-03 11:53:16 +0200

    update fetchposts function

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,62 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import config from '../../config';
 import Page from '../../components/Page';
 import InputForm from '../../components/InputForm';
 import Headline from '../../components/Headline';
 import Spinner from '../../components/Spinner';
 
 
-const limit = '100';
-let total = 500;
+const NUM_POSTS_TO_FETCH = 500;
+const MAX_NUM_POSTS_PER_PAGE = 100;
+const MAX_NUM_PAGES = NUM_POSTS_TO_FETCH / MAX_NUM_POSTS_PER_PAGE;
 
-function fetchData(slugValue, dataArray = [], afterValue = '') {
-  let after = afterValue;
-  let newAfter = '';
-  const loopData = dataArray;
+/**
+ * The reddit endpoint used to fetch the top posts uses pagination. We can fetch a maximum
+ * number of 100 posts per page. In order to fetch the first 500 posts we use this recursive
+ * function. Until the last page or the required number of posts has been reached we continue
+ * to fetch more posts.
+ *
+ * @param {string} subredditName the name of the subreddit
+ * @param {string} after the id of the last post used for pagination
+ * @param {number} page the current page being fetched
+ */
 
-  const url = `${config.api_url}/r/${slugValue}/top.json?&after=${afterValue}&limit=${limit}`;
+async function fetchPosts(subredditName, after = null, page = 1) {
+  let url = `https://www.reddit.com/r/${subredditName}/top.json?t=year&limit=${MAX_NUM_POSTS_PER_PAGE}`;
+  if (after) {
+    url += `&after=${after}`;
+  }
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      newAfter = json.data.after;
+  const response = await fetch(url);
+  const { data } = await response.json();
 
-      // check to stop loop when equal or null
-      if (newAfter !== afterValue && newAfter != null) {
-        after = newAfter;
-        loopData.push(...json.data.children);
-      } else {
-        // set to finish loop in next step
-        total = loopData.length;
-      }
-    })
-    .then(() => {
-      if (loopData.length < total) {
-        fetchData(slugValue, loopData, after);
-      }
-    })
-    // eslint-disable-next-line no-console
-    .catch((error) => console.log(`${error.message}`));
+  const posts = data.children;
+  const isLastPage = data.dist < MAX_NUM_POSTS_PER_PAGE;
+  const requiredNumOfPostsReached = page === MAX_NUM_PAGES;
 
-  return loopData;
+  if (isLastPage || requiredNumOfPostsReached) {
+    return posts;
+  }
+
+  const nextPosts = await fetchPosts(subredditName, data.after, page + 1);
+  return posts.concat(nextPosts);
 }
+
 
 function Search() {
   const { slug } = useParams();
 
+  // eslint-disable-next-line no-unused-vars
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
-    const newData = fetchData(slug);
-    setData(newData);
+    fetchPosts(slug)
+      .then((posts) => setData(posts))
+      .then(() => setLoading(false))
+      // eslint-disable-next-line no-console
+      .catch((error) => console.log(error.message));
   }, [slug]);
-
-  useEffect(() => {
-    setLoading(false);
-  }, [data]);
 
   return (
     <Page>
@@ -69,21 +70,6 @@ function Search() {
       {
         loading && <Spinner />
       }
-
-      {/* just for test  */}
-      {/* not updating */}
-      {
-        data.map((item, i) => (
-          <div key={item.data.title}>
-            {i}
-            /
-            {item.data.title.trim(0, 100)}
-            /
-            { item.data.subreddit}
-          </div>
-        ))
-      }
-
     </Page>
   );
 }
